65fae36d63291e647733135349687672d7f87023
siqigang <siqigang@meituan.com>
2018-08-26 22:16:00 +0800

    project init

---
+++ src/test/java/com/wimux/breakstones/BreakstonesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wimux.breakstones;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BreakstonesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
