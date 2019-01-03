7fe57127e606bbd5aead771db539b2fa4956dfa7
siqigang <siqigang@meituan.com>
2019-01-03 17:54:43 +0800

    applicaiton location error

--- src/main/java/com/wimux/breakstones/ApplicationMain.java
+++ src/main/java/com/wimux/breakstones/ApplicationMain.java
@@ -3,13 +3,12 @@ package com.wimux.breakstones;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * @Author siqigang
  * @Date 2018-12-29 16:45
  */
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 @MapperScan(basePackages = {"com.wimux.breakstones.mapper"})
 public class ApplicationMain {
     public static void main(String[] args) {
--- src/main/java/com/wimux/breakstones/config/BeanConfig.java
+++ src/main/java/com/wimux/breakstones/config/BeanConfig.java
@@ -1,12 +1,8 @@
 package com.wimux.breakstones.config;
 
-import com.alibaba.druid.pool.DruidDataSource;
 import com.wimux.breakstones.stone.uniqueid.SnowFlakeFake;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
 
 /**
  * @Author siqigang
@@ -14,22 +10,6 @@ import javax.sql.DataSource;
  */
 @Configuration
 public class BeanConfig {
-
-    @Bean
-    @ConfigurationProperties(prefix="spring.datasource.druid") //加载时读取指定的配置信息,前缀为spring.datasource.druid
-    public DataSource druidDataSource() {
-        return new DruidDataSource();
-    }
-
-
-//    @Bean(name = "sqlSessionFactory")
-//    @Primary
-//    public SqlSessionFactory sqlSessionFactoryOne(@Qualifier("datasource") DataSource dataSource)throws Exception{
-//        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
-//        bean.setDataSource(dataSource);
-//        return bean.getObject();
-//    }
-
     @Bean
     SnowFlakeFake snowFlakeFake() {
         return new SnowFlakeFake(2, 3);
